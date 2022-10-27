dd27882f3c5c9d900120849ee8395a75be305887
tessvdsluis <95242888+tessvdsluis@users.noreply.github.com>
2022-10-27 12:23:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,27 @@
 // JavaScript Document
 console.log("hi");
+
+/******************************/
+/* menu openen & sluiten de MENU button */
+/******************************/
+
+// menu button opgeslagen als variabelen deButton
+var deButton = document.querySelector("header button");
+
+// menu-button luistert naar klik en voert dan een functie uit
+deButton.addEventListener("click", toggleMenu);
+
+// Nieuwe functie toggleMenu 
+function toggleMenu() {  
+  // nav opgeslagen als variabelen
+  var deNav = document.querySelector("nav");
+  // nav krijgt class toonMenu
+  deNav.classList.toggle("toonMenu");
+}
+
+
+
+
+
+
+
